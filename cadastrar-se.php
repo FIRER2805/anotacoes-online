7e676fd277d721426d41def244364ec9b3fc1339
FIRER2805 <73549224+FIRER2805@users.noreply.github.com>
2022-09-29 18:32:40 -0300

    cadastrar-se.php

---
+++ cadastrar-se.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Cadastrar-se</title>
+	</head>
+	<body>
+		<h1>Cadastrar-se</h1>
+		<!--Formulario de cadastro-->
+		<form action="cadastros.php" method="post">
+			nome:
+			<input type="form" name="nome_cadastro">
+			e-mail:
+			<input type="form" name="e-mail_cadastro">
+			senha:
+			<input type="form" name="senha_cadastro">
+			<button type="submit">Cadastrar-se</button>
+		</form>
+	</body>
+</html>
