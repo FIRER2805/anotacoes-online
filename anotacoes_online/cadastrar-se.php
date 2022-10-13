05e616b45263cf5588898fac62431b189dcce6a5
FIRER2805 <73549224+FIRER2805@users.noreply.github.com>
2022-10-13 17:16:09 -0300

    Add files via upload

--- anotacoes_online/cadastrar-se.php
+++ anotacoes_online/cadastrar-se.php
@@ -3,34 +3,28 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="estilo/estilo.css" rel="stylesheet">
 		<title>Cadastrar-se</title>
 	</head>
 	<body>
-		<div class = "tela">
-			<div class="conteudo">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+		<div class="conteudo">
 				<h1 class = "logo">Cadastrar-se</h1>
 				<!--Formulario de cadastro-->
 				<form action="cadastros.php" method="post">
+					<div class="texto">nome:</div>
+					<input type="form" name="nome_cadastro" class ="input_texto">
 					<div class="campo_texto">
-						<span>nome:</span>
-						<input type="form" name="nome_cadastro" class ="input_texto">
-					</div>
-					<br>
-					<div class="campo_texto">
-						<span>e-mail:</span>
-						<input type="form" name="e-mail_cadastro" class ="input_texto">
-					</div>
-					<br>
-					<div class="campo_texto">
-						<span>senha:</span>
-						<input type="form" name="senha_cadastro" class ="input_texto">
-					</div>
-					<br>
-					<button type="submit" class="btn btn-outline-danger">Cadastrar-se</button>
-					<!-- div para finalizar o fluxo -->
-					<div class="break"></div>
+					<div class="texto">e-mail:</div>
+					<input type="form" name="e-mail_cadastro" class ="input_texto">
+					<div class="texto">senha:</div>
+					<input type="form" name="senha_cadastro" class ="input_texto">
+					<button type="submit" class="btn btn-outline-primary">Cadastrar-se</button>
 				</form>
-			</div>
+				<div class="botao_voltar">
+					<a class="btn btn-outline-danger" href="index.php">voltar</a>
+				</div>
 		</div>
 	</body>
 </html>
--- anotacoes_online/index.php
+++ anotacoes_online/index.php
@@ -5,23 +5,29 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Anotações</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="estilo/estilo.css" rel="stylesheet">
 
 	</head>
 	<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 
-		<h1 class="">Anotações online</h1>
-		<!--Formulario de login-->
-		<form action="login.php" method="post">
-			e-mail:
-			<input type="form" name="e-mail">
-			senha:
-			<input type="password" name="senha">
-			<button type="submit" class="btn btn-outline-info">Fazer Login</button>
-		</form>
+		<div class="conteudo">
+			<h1 class="logo">Anotações online</h1>
+			<!--Formulario de login-->
+			<form action="login.php" method="post">
+				<div class="texto">e-mail:</div>
+				<input type="form" name="e-mail">
+				<div class="texto">senha:</div>
+				<input type="password" name="senha">
+				<div class="btn-group">
+					<button type="submit" class="btn btn-outline-primary">Fazer Login</button>
+				</div>
+			</form>
 
-		<!-- botão para cadastro -->
-		<a href="cadastrar-se.php" class="btn btn-outline-primary">Cadastrar-se</a>
+			<!-- botão para cadastro -->
+			<div class="botao">
+				<a href="cadastrar-se.php" class="btn btn-outline-primary">Cadastrar-se</a>
+			</div>
 		<?php
 		// se houver erro, ira informar uma mensagem personalizada
 		if(array_key_exists('erro',$_GET))
@@ -37,5 +43,6 @@
 				break;
 			}
 		}?>
+		</div>
 	</body>
 </html>
