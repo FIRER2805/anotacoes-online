01b55e493ac1cdd0bfe71ea13a70ed01a1c34141
FIRER2805 <73549224+FIRER2805@users.noreply.github.com>
2022-10-13 21:17:13 -0300

    Add files via upload

--- anotacoes_online/criar_anotacao.php
+++ anotacoes_online/criar_anotacao.php
@@ -5,14 +5,24 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>fazer anotação</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="estilo/estilo_home.css" rel="stylesheet">
+		<link href="estilo/estilo.css" rel="stylesheet">
 	</head>
 	<body>
-		<form action="anotacoes.php" method="post">
-			titulo:
-			<input type="text" name="titulo">
-			descrição:
-			<input type="text" name="descricao">
-			<button type="submit">salvar</button>
-		</form>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+		<div class="conteudo">
+			<h1 class="logo">Anotação</h1>
+			<form action="anotacoes.php" method="post">
+				<div>titulo:</div>
+				<input type="text" name="titulo">
+				<div>descrição:</div>
+				<input type="text" name="descricao">
+				<button type="submit" class="btn btn-outline-primary btn-lg">salvar</button>
+			</form>
+				<div class="botao_voltar">
+					<a class="btn btn-outline-danger btn-lg" id="botao_voltar_criar_anotacao" href="home.php">voltar</a>
+				</div>
+		</div>
 	</body>
 </html>
--- anotacoes_online/mostrar_anotacoes.php
+++ anotacoes_online/mostrar_anotacoes.php
@@ -5,28 +5,39 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Anotações</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="estilo/estilo_home.css" rel="stylesheet">
 	</head>
 	<body>
-		<?php
-		// abre o arquivo
-		$arquivo = fopen('../../anotações_online_scripts/anotacoes.txt', 'r');
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+		<div class="conteudo">
+			<h1 class="logo">Anotações</h1>
+			<div class="anotacoes">
+				<?php
+				// abre o arquivo
+				$arquivo = fopen('../../anotações_online_scripts/anotacoes.txt', 'r');
 
-		// percorre o arquivo enquanto tiver linha
-		while(!feof($arquivo))
-		{
-			// sobre-escreve o array
-			$anotacao = explode('|',fgets($arquivo));
-			if(count($anotacao) == 3)
-			{
-				$email = substr($anotacao[2],0,-2);
-				if($_SESSION['email'] == $email)
+				// percorre o arquivo enquanto tiver linha
+				while(!feof($arquivo))
 				{
-			?>
-			<h2><?=$anotacao[0]?></h2>
-			<h3><?=$anotacao[1]?></h3>
-			<? }
-			}
-	} ?>
-	<a href="home.php">voltar</a>
+					// sobre-escreve o array
+					$anotacao = explode('|',fgets($arquivo));
+					if(count($anotacao) == 3)
+					{
+						$email = substr($anotacao[2],0,-2);
+						if($_SESSION['email'] == $email)
+						{
+					?>
+					<div class="anotacao">
+						<h2 class="titulo"><?=$anotacao[0]?></h2>
+						<h3 class="descricao"><?=$anotacao[1]?></h3>
+					</div>
+					<? }
+					}
+				} ?>
+				<div class="break"></div>
+				<a href="home.php" class="btn btn-outline-danger btn-lg" id="botao_sair_anotacoes">voltar</a>
+			</div>
+		<div>
 	</body>
 </html>
